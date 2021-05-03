ab76686b910105aaf4b6163b13f115b05bc614dd
Georgy <georgynogovicin@yandex.ru>
2021-05-03 23:28:36 +0300

    initial

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+import classes from './Header.module.scss';
+
+const Header = () => {
+    return (
+        <header className={classes.header}>
+            <div className={classes.container}>
+                Hello
+            </div>
+        </header>
+    )
+}
+
+export default Header;
